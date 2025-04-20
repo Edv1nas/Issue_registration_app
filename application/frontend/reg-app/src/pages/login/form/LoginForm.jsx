88fff901b65e37b8fda4793bfbf7f4beac77ce96
Edv1nas <eafarjancas@gmail.com>
2025-04-20 20:18:39 +0300

    fixing connect v43

--- application/frontend/reg-app/src/pages/login/form/LoginForm.jsx
+++ application/frontend/reg-app/src/pages/login/form/LoginForm.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useNavigate } from 'react-router-dom';
-import 
 
 const LoginForm = () => {
        const [username, setUsername] = useState('');
